2db84a7f59ec24a0c25156d1669c40be2e85f482
jordydev1993 <jordy.facebook.1993@gmail.com>
2026-04-25 12:12:42 -0300

    nuevos updates

--- interfaz-diseño/src/app/components/design-system/index.ts
+++
@@ -1,7 +0,0 @@
-export { GlassCard, glassCardVariants } from './GlassCard';
-export type { GlassCardProps } from './GlassCard';
-
-export { ColorSwatch } from './ColorSwatch';
-
-export { IconButton, iconButtonVariants } from './IconButton';
-export type { IconButtonProps } from './IconButton';
--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  webpack: (config) => {
+    config.watchOptions = {
+      ignored: ['**/interfaz-diseño/**', '**/.next/**', '**/node_modules/**'],
+    }
+    return config
+  },
   images: {
     remotePatterns: [
       {
